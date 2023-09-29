7a0f4d23a8e4e81a3439aba6cca166c95e28bc73
Mumair567 <146439675+Mumair567@users.noreply.github.com>
2023-09-29 19:26:56 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+print(2+3*4)
+#float division use only one "/"
+print(4/2)
+#integer division use two operator "//"
+print(4//2)
+#for exponenet use "**"
+print(2**3)
+#for underoot take exponent of that number multiply with 0.5 as 0.5 equal to 1/2
+print( 2**0.5)# for underoot 2
+#underro2 answer is lenghthy if we want too round of that number use round function
+#with that particular number after comma means to what extent you want to round number
+print(round(2**0.5,3))
+# In math we have BODMAS rule to calculate the equations but in python 
+#If we have to calculate lengthy calculation then we use precedence rule and Associativity rule
+#In precedence rule first of all parenthesis will be solved then exponent have higest precedence
+# *,/,//,% they have same precedence and associativity from left to right
+#example print((2+3)*5/6%2)
+#first parenthesis will be solved then multilpy that with 5 then /6 then %2
+# +,- Associativity from left to right
+# Exponent associativity from right to left
+print(2**3**2)
+# from right side 3^2 means 9 and then 2^9 equal to 512
+
+
+
